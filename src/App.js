838dbe927f8b8a7beaa65a2794e477b883e6b3eb
Johann <johannth88@gmail.com>
2020-12-02 15:53:14 +0000

    Added initial button & mobile logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Currency from './components/Currency';
 import News from './components/News';
-import './css/App.scss'
 
 function App() {
-  return (
+  const [isMobile, setIsMobile] = useState(false);
+  const [visibleOnMobile, setVisibleMobile] = useState(<Currency />);
 
+  // Runs when component mounts
+  useEffect(() => {
+    console.log('In use effect');
+    function updateWidth(ev) {
+      if(window.innerWidth >= 600) {
+        setIsMobile(false);
+      } else {
+        setIsMobile(true);
+      }
+    }
+    window.addEventListener('resize', updateWidth);
+    updateWidth();
+    return () => {
+      setVisibleMobile(null);
+      window.removeEventListener('resize', updateWidth);
+    }
+  }, []);
+
+  return (
     <div>
-      <Currency />
-      <News />
+      {isMobile ? (
+        <div>
+          <button>Menu</button>
+          <ul>
+            <li onClick={() => setVisibleMobile(<News />)}>News</li>
+            <li onClick={() => setVisibleMobile(<Currency />)}>Exhange</li>
+            <li>Chat</li>
+          </ul>
+          {
+            visibleOnMobile
+          }
+        </div>
+      ) : (
+        <div>
+          <Currency />
+          <News />
+        </div>
+      )
+    }
     </div>
   );
 }
